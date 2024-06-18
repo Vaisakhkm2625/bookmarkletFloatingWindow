4d7e6ae193c906a920772b54f6d04d4e720d4fe7
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-06-18 19:38:27 +0530

    fixed color issues

--- background.js
+++ background.js
@@ -18,6 +18,35 @@
 //});
 
 
+//// background.js
+//
+//// Listener for messages from the content script
+//chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//    if (message.action === 'getJavaScriptBookmarks') {
+//        // Fetch all bookmarks
+//        chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+//            const javascriptBookmarks = [];
+//            // Recursive function to traverse bookmarks tree
+//            function traverseBookmarks(bookmarkNodes) {
+//                for (const node of bookmarkNodes) {
+//                    if (node.url && node.url.startsWith('javascript:')) {
+//                        javascriptBookmarks.push(node);
+//                    }
+//                    if (node.children) {
+//                        traverseBookmarks(node.children);
+//                    }
+//                }
+//            }
+//            traverseBookmarks(bookmarkTreeNodes);
+//            // Send the JavaScript bookmarks back to the content script
+//            sendResponse({ bookmarks: javascriptBookmarks });
+//        });
+//        return true; // Indicates that the response is sent asynchronously
+//    }
+//});
+
+
+
 // background.js
 
 // Listener for messages from the content script
@@ -30,7 +59,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             function traverseBookmarks(bookmarkNodes) {
                 for (const node of bookmarkNodes) {
                     if (node.url && node.url.startsWith('javascript:')) {
-                        javascriptBookmarks.push(node);
+                        javascriptBookmarks.push({ url: node.url, name: node.title });
                     }
                     if (node.children) {
                         traverseBookmarks(node.children);
--- content.js
+++ content.js
@@ -7,29 +7,38 @@ container.id = 'floating-button-container';
 document.body.appendChild(container);
 
 
-function fetchBookmarks() {
+// Define the bookmarklets
+const internalBookmarklets = [
+    {
+        name: 'Reload',
+        url: "javascript:(function()%7Bvar%20w%20%3D%20window.open(window.location.href)%3BsetInterval(function()%7Bw.window.location.reload()%7D%2C10000)%7D)()",
+        internal: true
+    },
+    {
+        name: 'Change Background Color',
+        url: 'javascript:(function()%7Bdocument.body.style.backgroundColor%20%3D%20%22lightblue%22%3B%7D)()%3B',
+        internal: true
+    }
+];
+let bookmarklets = internalBookmarklets;
+
+function fetchJavaScriptBookmarks() {
     chrome.runtime.sendMessage({ action: 'getJavaScriptBookmarks' }, (response) => {
         if (response && response.bookmarks) {
-            // Process the bookmarks
-            console.log(response.bookmarks);
+            // Process the bookmarks with "javascript:" URLs
+            console.log('JavaScript bookmarks:', response.bookmarks);
+            // Example of using the fetched bookmarks
+            response.bookmarks.forEach(bookmarklet => {
+                console.log(`Name: ${bookmarklet.name}, URL: ${bookmarklet.url}`);
+            });
+            bookmarklets = internalBookmarklets.concat(response.bookmarks);
+            updateFloatingContainer(document.querySelector('#draggableDivcontent'), bookmarklets);
         }
     });
 }
 
-fetchBookmarks();
-
-// Define the bookmarklets
-const bookmarklets = [
-    {
-        name: 'Google Search',
-        code: "javascript:(function()%7Balert('hello% 20world')%3B%7D)()"
-    },
-    {
-        name: 'Change Background Color',
-        code: 'document.body.style.backgroundColor = "lightblue";'
-    }
-];
-
+// Call the function to fetch JavaScript bookmarks
+fetchJavaScriptBookmarks();
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -96,19 +105,28 @@ function createFloatingContainer() {
 
 function updateFloatingContainer(div, bookmarklets) {
 
-    bookmarklets.forEach(bookmarklet => {
+    console.log("updating")
+    console.log(bookmarklets)
 
-        //const p = document.createElement('p');
-        const a = document.createElement('a');
-        a.innerHTML = bookmarklet.name;
-        a.classList.add("dragaButton");
-        a.href = bookmarklet.code;
+    if (typeof (div) != 'undefined' && div != null) {
 
-        //p.appendChild(a);
-        div.appendChild(a);
+        bookmarklets.forEach(bookmarklet => {
+
+            //const p = document.createElement('p');
+            const a = document.createElement('a');
+            a.innerHTML = bookmarklet.name;
+            if (bookmarklet.internal && bookmarklet.internal === true) {
+                a.style.backgroundColor = "#007bff";
+            }
+            //a.classList.add("dragaButton");
+            a.href = bookmarklet.url;
+
+            //p.appendChild(a);
+            div.appendChild(a);
 
 
-    });
+        });
+    }
 }
 
 function main() {
@@ -121,8 +139,11 @@ function main() {
     let content = document.querySelector('#draggableDivcontent')
     console.log(content)
 
-    content.classList.add("vertical-btn-group");
 
+    div.style.top = (window.innerHeight - 200) + "px";
+    div.style.left = (window.innerWidth - 200) + "px";
+
+    content.classList.add("vertical-btn-group");
 
     updateFloatingContainer(content, bookmarklets)
 
